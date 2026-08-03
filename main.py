1fec7a47a6ff1b4a3f5640f0205bc10323568260
Arpit <arpitgangwani517@gmail.com>
2026-08-03 08:03:07 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validtion import DataValidation
 import sys
 from networksecurity.exception.exception import Networksecurityexception
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
@@ -12,6 +13,16 @@ if __name__=='__main__':
         dataingestionobj=DataIngestion(data_ingestion_config=dataingestionconfigobj)
         logging.info("Initiate the data ingestion")
         data_ingestion_artifact=dataingestionobj.initiate_data_ingestion()
+        logging.info("Data Ingestion completed!")
         print( data_ingestion_artifact)
+
+        data_validation_config_obj = DataValidationConfig(
+            training_pipeline_config=trainingpipelineobj
+        )
+        data_validation_obj=DataValidation(data_validation_config=data_validation_config_obj,data_ingestion_artifact=data_ingestion_artifact)
+        logging.info("Initiate the data validation")
+        data_validation_artifact=data_validation_obj.initiate_data_validation()
+        logging.info("Data Validation completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise Networksecurityexception(e,sys)
--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -11,7 +11,7 @@ class Networksecurityexception(Exception):
         self.filename=exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return "Error occured in python script name [{0} ]line number [{1}] error message [{2}]".format(
+        return "Error occured in python script name [{0} ]line number [{1}] error message [{2}]\n".format(
             self.filename,self.lineno,str(self.error_message)
         )
 
